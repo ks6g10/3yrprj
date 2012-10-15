067eebdc13604c4f5c0ea0ae72a1b6a92a7e669c
Kim Svensson <ks6g10@soton.ac.uk>
2012-10-15 20:25:59 +0100

    Further development on the dynamic alghrithm, started the readme of the implementation details.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 3yrprj
 ======
+
+How is a set represented?
+
+
+There will be two definitions, an asset set, where you define your assets as an array(could be removed in future). 
+And a member-set/subset where you define which members this set contains from set A.
+A member-set/subset will be defined by n numbers of intigers where one integer represent 32 members of that set.
+
+If the set A(represented as an array) =  apple, mapple, banana, potato where apple have the index of 0, mapple 1... in the array.
+A subset/member-set of A containing banana and apple, the integer representing which members contained would have a value of 5 as 0101 in binary is equal to 5.
+So the least significant digit of the member-set represent the first element in the array A.
+
+The reason behind this design choise is that 
+ 
--- dynprog.c
+++ dynprog.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
 
-/*                  1         2      4       8        16*/
-char * assets[5] = {"apple","mapple","pear","potato","asdf"};
-unsigned int set1 = 1; //apple pear potato
-unsigned int set2 = 6; // mapple pear potato
+struct _bid {
+	unsigned int value;
+	unsigned int comb;
+} typedef bid;
 
 
+/*0000 0 0
+ *0001 1 1 1
+ *0010 1 2 2
+ *0011 2 3 2
+ *0100 1 4 4
+ *0101 2 5 1
+ *0110 2 6 3
+ *0111 3 7
+ *1000 1 8 8
+ *1001 2 9 1
+ *1010 2 10 2
+ *1011 3 11
+ *1100 2 12 
+ *1101 3 13
+ *1110 3 14
+ *1111 4 15
+ *10000  
+ *
+ */
+
+/*                    1         2        4       8        16*/
+#define MAX 8
+char * assets[3] = {"apple","mapple","potato"};
+/*		      0 1 2 3 4 5 6 7 8			*/
+unsigned int bids[MAX] {0,2,3,6,2,3,5,7}; 
+unsigned int oset[MAX] {0,1,2,3,4,5,6,7};
+unsigned int f[MAX] = {0};
+unsigned int O[MAX] = {0};
+
+int[] set_singleton_bid(register int[] _bids) {
+	register unsigned int i;
+	register unsigned int _f[MAX];
+	for(i =1; i< MAX; i*=2) {
+		_f[i] = _bids[i]
+	}
+	return _f;
+}
+  
 inline unsigned int intersect(unsigned int seta, unsigned int setb) {
      return (seta & setb);
 }
@@ -23,16 +61,33 @@ inline unsigned int cardinality(unsigned int seta) {
      return __builtin_popcount(seta);
 }
 
+/*
+ *f[]
+ *O[]
+ *	
+ */
 
  int main(void) {
-      unsigned int i;
-      for(i = 1; i < 8; i++) {
-      
-      unsigned     int z = intersect(i,i-1);
-      unsigned     int x = _union(i,i-1);
-      unsigned     int f = setdiff(i,i-1);
-      unsigned int t = cardinality(i);
-      printf("i %d \tinter %u\t union %u\t diff %u\t card %u\n",i,z,x,f,t);
-      }
-      return 0;
+	 /*1.*/
+	 f = set_singleton_bid(bids);
+	 O = set_singleton_bid(oset);
+	 /*2.*/
+	 register unsigned int i;
+	 for(i = 2; i <MAX; i++) {
+
+
+
+
+	 }
+	 return 0;
+	 unsigned int i;
+	 for(i = 1; i < 8; i++) {
+		 
+		 unsigned     int z = intersect(i,i-1);
+		 unsigned     int x = _union(i,i-1);
+		 unsigned     int f = setdiff(i,i-1);
+		 unsigned int t = cardinality(i);
+		 printf("i %d \tinter %u\t union %u\t diff %	u\t card %u\n",i,z,x,f,t);
+	 }
+	 return 0;
 } 
