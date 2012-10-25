873af165678725509cab7c07f1e7722c815dff72
Kim Svensson <ks6g10@soton.ac.uk>
2012-10-25 13:21:45 +0100

    Old revision of the code, this is used for the time benchmark, will do the same for further revisions

--- dynprog.c
+++ dynprog.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <strings.h> // for ffs
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define ITEMS 20
+#define MAX (2 << (ITEMS-1))
+#if ITEMS < 8
+#define dint uint8_t
+#elif ITEMS < 16
+#define dint uint16_t
+#elif ITEMS < 32
+#define dint uint32_t
+#elif ITEMS > 32
+#define dint uint64_t
+#endif
+
 struct _bid {
-	unsigned int value;
-	unsigned int comb;
+	dint value;
+	 dint comb;
 } typedef bid;
-
-
 /*0000 0 0
  *0001 1 1 1
  *0010 1 2 2
@@ -40,30 +54,31 @@ struct _bid {
  */
 
 /*                    1         2        4       8        16*/
-#define MAX 8
+
 char * assets[3] = {"apple","mapple","potato"};
 /*		           0 1 2 3 4 5 6 7 8			*/
-//unsigned int bids[MAX] ={0,2,3,6,4,7,6,9}; //conf 5 and 2
-//unsigned int bids[MAX] =  {0,2,3,6,4,6,6,9}; //conf 3 and 4
-unsigned int bids[MAX] =  {0,20,3,6,4,6,10,9}; //conf 1 and 6
-unsigned int wopt[MAX] = {0};
-unsigned int f[MAX] = {0};
-unsigned int O[MAX] = {0};
+// dint bids[MAX] ={0,2,3,6,4,7,6,9}; //conf 5 and 2
+// dint bids[MAX] =  {0,2,3,6,4,6,6,9}; //conf 3 and 4
+// dint bids[MAX] =  {0,20,3,6,4,6,10,9}; //conf 1 and 6
+dint bids[MAX];
+ dint wopt[MAX] = {0};
+ dint f[MAX] = {0};
+ dint O[MAX] = {0};
   
-inline unsigned int intersect(unsigned int seta, unsigned int setb) {
+inline  dint intersect( dint seta,  dint setb) {
      return (seta & setb);
 }
 
-inline unsigned int _union(unsigned int seta, unsigned int setb) {
+inline  dint _union( dint seta,  dint setb) {
      return (seta | setb);
 }
 
-inline unsigned int setdiff(unsigned int seta, unsigned int setb) {
+inline  dint setdiff( dint seta,  dint setb) {
 
      return (seta & ~setb);
 }
 
-inline unsigned int cardinality(unsigned int seta) {
+inline  dint cardinality( dint seta) {
      return __builtin_popcount(seta);
 }
 
@@ -73,19 +88,35 @@ inline unsigned int cardinality(unsigned int seta) {
  *bids[]	
  */
 
+void gen_rand_bids() {
+	register dint i = 0;
+	printf("i =\t");
+	for(i = 1; i < MAX;i++) {
+		bids[i] = 1;//rand() % 10;
+		//printf("%u\t",i);
+	}
+	return;
+		printf("\n");
+		printf("val =\t");
+	for(i = 1; i < MAX;i++) {
+		printf("%u\t",bids[i]);
+	}
+	printf("\n");
+}
+
 struct _bnode{
      void * ptr[2];// = {NULL};
-     unsigned int value;
+      dint value;
 } typedef node;
 
 
 
 
 /*Bids are arranged like following: bid[n][0] is the set config bid[n][1] is the value*/
-node * parse_bids(int nitems, int nbids, int ** bids ) {
+node * parse_bids(dint nitems, dint nbids, dint ** bids ) {
      node * root = malloc(sizeof(node));
-     int i,b;
-     int tmp =0;
+     dint i,b;
+     dint tmp =0;
      node * curr;
      // go through all bids
      for(b = 0; b < nbids;b++) {
@@ -95,7 +126,7 @@ node * parse_bids(int nitems, int nbids, int ** bids ) {
 	  //go through all bits in config
 	  for(i=0;i<nitems;i++) {
 	       /* if the bit i is 1*/
-	       int bit = (1 & (tmp>> i));
+	       dint bit = (1 & (tmp>> i));
 	       if(curr->ptr[bit] == NULL) {
 		    curr->ptr[bit] = malloc(sizeof(node));
 	       }
@@ -110,8 +141,11 @@ node * parse_bids(int nitems, int nbids, int ** bids ) {
      }
 }
 
+
+
 void printfo() {
-     unsigned int i;
+	return;
+      dint i;
      printf("i\t");
      for(i =1; i < MAX; i++) 
 	  printf("%u\t",i);
@@ -127,7 +161,7 @@ void printfo() {
 }
 
 inline void set_singleton_bid() {
-  register unsigned int i;
+  register  dint i;
   for(i =1; i< MAX; i*=2) {
        f[i] = bids[i];
        if(bids[i] > 0)
@@ -135,12 +169,12 @@ inline void set_singleton_bid() {
   }
 }
 
-unsigned int max(unsigned conf) {
-     register unsigned int card = cardinality(conf)/2;
-     unsigned int i;
-     unsigned int max = 0;
-     unsigned int set = 0;
-     unsigned int tmp = 0;
+dint max(dint conf) {
+     register  dint card = cardinality(conf)/2;
+      dint i;
+      dint max = 0;
+      dint set = 0;
+      dint tmp = 0;
      for(i=1;i<MAX;i++) {
 	  if(cardinality(i) > card)
 	       continue;
@@ -162,24 +196,24 @@ struct _bleaf {
 	  void * ptr0;// = NULL;
 	  void * ptr1;// = NULL;
      };
-     unsigned int value;
+      dint value;
 } typedef leaf;
 
 struct _stack {
-	unsigned int conf;
+	 dint conf;
 	struct _stack * next;
 } typedef stack;
 
 void parse_wopt(void) {
-     unsigned int c;
      //wopt at start contain MAX at wopt[0] which is the combination that goes in bids[wopt[n]]
      stack * root = malloc(sizeof(stack));
-     root->conf = MAX-1;
+     //DO NOT REMOVE -1
+     root->conf = (MAX)-1;
      stack * curr = root;
      while(curr != NULL) {
-	     unsigned int conf = curr->conf;
+	      dint conf = curr->conf;
 	     if(conf != O[conf]) {
-		     unsigned int diff = setdiff(conf,O[conf]);
+		     dint diff = setdiff(conf,O[conf]);
 		     curr->conf = O[conf];
 		     stack * tmp = malloc(sizeof(stack));
 		     tmp->conf = diff;
@@ -197,31 +231,32 @@ void parse_wopt(void) {
      }
 }
 
-int main(void) {
-      register unsigned int i;
+dint main(void) {
+      printf("max %d\n", MAX);
+      register dint i;
       /*1.*/
+      gen_rand_bids();
       set_singleton_bid();
-       printfo();
+      //printfo();
       /*2.*/
-      //register unsigned int i;
       for(i = 2; i <MAX; i++) {
-	   unsigned int c;
-	   for(c = 1; c < MAX; c++) {
-		if(cardinality(c) == i && bids[c] > 0) {
-		     printf("hit card %u \n",i);
-		     unsigned int tmpset = max(c);
-		     if(f[c] >= bids[c]) {//b
-			     O[c] = tmpset;//net to set
-		     }
-		     else {//c
-			  f[c] = bids[c];
-			  O[c] = c;
-		     }
-		     printfo();
-		}
-	   }
+	       dint c;
+	      for(c = 1; c < MAX; c++) {
+		      if(cardinality(c) == i && bids[c] > 0) {
+			      //printf("hit card %u \n",i);
+			       dint tmpset = max(c);
+			      if(f[c] >= bids[c]) {//b
+				      O[c] = tmpset;//net t	o set
+			      }
+			      else {//c	
+				      f[c] = bids[c];
+				      O[c] = c;
+			      }
+			      //printfo();
+		      }
+	      }
       }
-
+      	     	      	     
       parse_wopt();
       //printfo();
 
@@ -229,11 +264,11 @@ int main(void) {
       /*Testing facility*/
 	 for(i = 1; i < 8; i++) {
 		 
-		 unsigned     int z = intersect(i,i-1);
-		 unsigned     int x = _union(i,i-1);
-		 unsigned     int f = setdiff(i,i-1);
-		 unsigned int t = cardinality(i);
-		 printf("i %d \tinter %u\t union %u\t diff %	u\t card %u\n",i,z,x,f,t);
+		 dint z = intersect(i,i-1);
+		 dint x = _union(i,i-1);
+		 dint f = setdiff(i,i-1);
+		 dint t = cardinality(i);
+		 printf("i %u \tinter %u\t union %u\t diff %u\t card %u\n",i,z,x,f,t);
 	 }
 	 return 0;
 } 
