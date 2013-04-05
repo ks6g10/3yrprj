d98db5d13af3b6629afe2641ee526540fbe8a2aa
Kim Svensson <ks6g10@soton.ac.uk>
2013-04-05 20:01:13 +0100

    more stuff, refactoring

--- read.c
+++ read.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define WORDSIZE (32)
+
 struct bid {
 	double offer;
 	unsigned int id;
@@ -32,68 +34,143 @@ struct allocation {
 };
 
 
-unsigned int ints =0;
-unsigned int * MASK = void;
+/* unsigned int ints =0; */
+/* unsigned int * MASK = void; */
 
-double h(struct allocation * pi,struct bid2 ** bins,unsigned int * bin_counts) {
-	unsigned int u_bound = 0;
-//	unsigned int free[ints];
-	unsigned int * pi_conf = pi->conf;
-	int x;
-//	double * means[goods];
-	double upper_bound =0;
-	unsigned int tmp;
-	for(x = 0; x < ints; x++) {
-		tmp  = ~pi_conf[x] & MASK[x];
-		while(tmp) {
-			unsigned int index = __builtin_ffs(tmp) -1;
-			tmp  &= ~(1 << index);
-			upper_bound +=  v(bins[x],pi_conf,bin_counts[x]);
-		}
-	}
-	return u_bound;
+/* double h(struct allocation * pi,struct bid2 ** bins,unsigned int * bin_counts) { */
+/* 	unsigned int u_bound = 0; */
+/* //	unsigned int free[ints]; */
+/* 	unsigned int * pi_conf = pi->conf; */
+/* 	int x; */
+/* //	double * means[goods]; */
+/* 	double upper_bound =0; */
+/* 	unsigned int tmp; */
+/* 	for(x = 0; x < ints; x++) { */
+/* 		tmp  = ~pi_conf[x] & MASK[x]; */
+/* 		while(tmp) { */
+/* 			unsigned int index = __builtin_ffs(tmp) -1; */
+/* 			tmp  &= ~(1 << index); */
+/* 			upper_bound +=  v(bins[x],pi_conf,bin_counts[x]); */
+/* 		} */
+/* 	} */
+/* 	return u_bound; */
+/* } */
+
+/* oid init_mask(void) { */
+/* 	MASK = calloc(sizeof(int),ints); */
+/* 	int x; */
+/* 	unsigned int mod,tmp; */
+/* 	for(x =0; x < ints) { */
+/* 		if(x < (ints-1)) { */
+/* 			MASK[x] = ~0; */
+/* 		} else { */
+/* 			mod = NGOODS % (INT); */
+/* 			tmp = (1 << mod)-1; */
+/* 			MASK[x] = tmp; */
+/* 		} */
+/* 	}	       	 */
+/* } */
+
+/* int do_not_intersect(struct bid2 * a_bid, unsigned int * conf) { */
+/* 	int x; */
+/* 	unsigned int tmp; */
+
+/* 	for(x = 0; x < ints; x++)  { */
+/* 		tmp = a_bid->conf[x] & conf[x]; */
+/* 		if(tmp) { */
+/* 			return 0; */
+/* 		} */
+/* 	} */
+/* 	return 1; */
+/* } */
+
+/* double v(struct bid2 * bin,unsigned int * conf,unsigned int bin_count) { */
+/* 	if(!bin_count) { */
+/* 		return 0.0; */
+/* 	} */
+/* 	int x; */
+/* 	for(x =0; x < bin_count;x++) { */
+/* 		if(do_not_intersect( bin[x], conf )) { */
+/* 			return  */
+/* 		} */
+/* 	} */
+/* } */
+
+unsigned int next_index(unsigned int a_index) {		
+	return __builtin_ffs(a_index) -1;
+//	tmp  &= ~(1 << index);
+//	upper_bound +=  v(bins[x],pi_conf,bin_counts[x]);
+	
 }
 
-oid init_mask(void) {
-	MASK = calloc(sizeof(int),ints);
-	int x;
-	unsigned int mod,tmp;
-	for(x =0; x < ints) {
-		if(x < (ints-1)) {
-			MASK[x] = ~0;
-		} else {
-			mod = NGOODS % (INT);
-			tmp = (1 << mod)-1;
-			MASK[x] = tmp;
+double * calc_score(unsigned int total_goods,
+		    unsigned int bids,
+		    unsigned int ints,
+		    struct bid * src_ptr) {
+	double * score =  malloc(sizeof(score)*total_goods);
+	unsigned int * score_total = malloc(sizeof(score_total)*total_goods);
+	unsigned int * score_c = malloc(sizeof(score_c)*total_goods);
+	int x,y;
+	for(x = 0;x < total_goods;x++) {
+		score[x] = 0.0;
+		score_total[x] = 0;
+		score_c[x] = 0;		
+	}
+	for(x=0; x < bids; x++) {
+		for(y=0;y<ints;y++) {
+			unsigned int conf = src_ptr->conf[y];
+			while(conf) {
+				unsigned int index = next_index(conf);
+				conf &= ~(1 << index);
+				score_total[(y*WORDSIZE + index)] += src_ptr->goods;
+				score_c[(y*WORDSIZE + index)]++;
+			}
 		}
-	}	       	
+		src_ptr++;
+	}
+	for(x = 0;x < total_goods;x++) {
+		score[x] =((double) score_c[x])/((double) score_t[x]);
+	}
+	free(score_c);
+	free(score_total);
+	return score;
 }
 
-int do_not_intersect(struct bid2 * a_bid, unsigned int * conf) {
-	int x;
-	unsigned int tmp;
+struct bid2 ** assign_bids_to_bins(unsigned int total_goods,
+				   unsigned int bids,
+				   unsigned int ints,
+				   struct bid * src_ptr) {
 
-	for(x = 0; x < ints; x++)  {
-		tmp = a_bid->conf[x] & conf[x];
-		if(tmp) {
-			return 0;
+	struct bid2 **bin =(struct bid2 **) malloc(sizeof(struct bid2 **)*(total_goods));
+	for(x = 0; x < total_goods;x++) {
+		(bin)[x] = malloc((sizeof(struct bid2)*bin_count[x]));
+		if(!(bin[x])) {
+			printf("Could not allocate memory as line %d\n",__LINE__);
+			exit(1);
 		}
+		printf("count = %u, bin %u\n",bin_count[x],x);
+		bin_count[x]=0;
 	}
-	return 1;
-}
+	int y;
 
-double v(struct bid2 * bin,unsigned int * conf,unsigned int bin_count) {
-	if(!bin_count) {
-		return 0.0;
-	}
-	int x;
-	for(x =0; x < bin_count;x++) {
-		if(do_not_intersect( bin[x], conf )) {
-			return 
+	struct bid2 * dst_ptr;// = *bin;
+	struct bid * src_ptr = bids_ptr;//NULL;// = *bin;
+	for(x = 0; x < bids;x++) {
+		//*src_ptr = bids_ptr[x];
+		unsigned int index = bin_count[src_ptr->bin];
+		bin_count[src_ptr->bin]++;
+		dst_ptr = bin[src_ptr->bin];
+		(dst_ptr[index]).offer = src_ptr->offer;
+		(dst_ptr[index]).id = src_ptr->id;
+		(dst_ptr[index]).average =(double) src_ptr->offer/((double) src_ptr->goods);
+		for(y = 0; y < ints;y++) {
+			(dst_ptr[index]).conf[y] = src_ptr->conf[y];
 		}
+		src_ptr++;
+		printf("id %u val %lf\n",(dst_ptr[index]).id,(dst_ptr[index]).offer);		
 	}
+	return bin;
 }
-
 
 int main(int argc, char *argv[])   {
 	const char * s_goods = "goods";
@@ -107,7 +184,7 @@ int main(int argc, char *argv[])   {
 	unsigned int bids = 0;
 	unsigned int dummy = 0;
 	unsigned int all = 0;
-	
+	unsigned int total_goods = 0;
 	
 	unsigned int *tmp = NULL;
 	struct bid * bids_ptr;
@@ -137,25 +214,26 @@ int main(int argc, char *argv[])   {
 			if(strncmp(line,s_dummy,strlen(s_dummy)) == 0) {
 				dummy = atoi(line+strlen(s_dummy)+1);
 				printf("Number of dummy %u\n",dummy);
-				ints = 1+(goods+dummy-1)/32;
+				ints = 1+(total_goods-1)/32;
 			}
+			total_goods = goods + dummy;
 			all = !!(goods && bids && dummy);
 		} 
 
 	}
 
-	bin_count = calloc(sizeof(bin_count),goods+dummy);
+	bin_count = calloc(sizeof(bin_count),total_goods);
 	tmp = malloc(sizeof(tmp)*ints);			
 	if(!(tmp) || !(bin_count)) {
 		printf("Could not allocate memory as line %d\n",__LINE__);
 		exit(1);
 	}
 
-	for(x =0;x < goods+dummy;x++) {
+	for(x =0;x < total_goods;x++) {
 		bin_count[x] = 0;
 	}
 
-	bids_ptr = calloc((sizeof(bids_ptr)+(1+(goods-1+dummy)/32)*sizeof(int)),bids);
+	bids_ptr = calloc((sizeof(bids_ptr)+(1+(total_goods-1)/32)*sizeof(int)),bids);
 	if(!(bids_ptr)) {
 		printf("Could not allocate memory as line %d\n",__LINE__);
 		exit(1);
@@ -221,8 +299,8 @@ int main(int argc, char *argv[])   {
 		
 	}
 	
-	struct bid2 **bin =(struct bid2 **) malloc(sizeof(struct bid2 **)*(goods+dummy));
-	for(x = 0; x < goods+dummy;x++) {
+	struct bid2 **bin =(struct bid2 **) malloc(sizeof(struct bid2 **)*(total_goods));
+	for(x = 0; x < total_goods;x++) {
 		(bin)[x] = malloc((sizeof(struct bid2)*bin_count[x]));
 		if(!(bin[x])) {
 			printf("Could not allocate memory as line %d\n",__LINE__);
@@ -235,6 +313,7 @@ int main(int argc, char *argv[])   {
 
 	struct bid2 * dst_ptr;// = *bin;
 	struct bid * src_ptr = bids_ptr;//NULL;// = *bin;
+	
 	for(x = 0; x < bids;x++) {
 		//*src_ptr = bids_ptr[x];
 		unsigned int index = bin_count[src_ptr->bin];
@@ -245,11 +324,12 @@ int main(int argc, char *argv[])   {
 		(dst_ptr[index]).average =(double) src_ptr->offer/((double) src_ptr->goods);
 		for(y = 0; y < ints;y++) {
 			(dst_ptr[index]).conf[y] = src_ptr->conf[y];
-
 		}
 		src_ptr++;
 		printf("id %u val %lf\n",(dst_ptr[index]).id,(dst_ptr[index]).offer);		
 	}
+	
+	double * score = calc_score(total_goods,bids,ints,bids_ptr);
 	free(bids_ptr);
 	//bids_ptr++;
 //	free(bids_ptr);
